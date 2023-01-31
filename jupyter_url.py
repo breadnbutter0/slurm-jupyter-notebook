44c2232c14a3a1823eea65dd1109a51c2d0a7814
Hansae Kim <kim4012@purdue.edu>
2023-01-31 13:45:12 -0500

    fixed print url and updated troubleshoot

--- jupyter_url.py
+++ jupyter_url.py
@@ -2,27 +2,28 @@ import os
 
 file_dir = os.path.dirname(os.path.realpath(__file__))
 #%%
-with open("{}/jupyter.log".format(file_dir)) as f:
-    lines=f.readlines()
-    
-hostname = lines[6][:-1]
-for i, line in enumerate(lines):
-    try:
-        host = line.split(' ')[1]
-        if i > 6 and host == hostname:
-            host_ip_address = line.split(' ')[0]
-        
-        if line == '    Or copy and paste one of these URLs:\n':
-            jupyter_vscode = lines[i+1][8:-1]
-            jupyter_local_url = lines[i+2][8:-1]
-    except:
-        pass
-    
-#%%
-ssh_command = lines[12].split(':')
-ssh_command[1] = host_ip_address
-
 try:
+    with open("{}/jupyter.log".format(file_dir)) as f:
+        lines=f.readlines()
+        
+    hostname = lines[6][:-1]
+    for i, line in enumerate(lines):
+        try:
+            host = line.split(' ')[1]
+            if i > 6 and host == hostname:
+                host_ip_address = line.split(' ')[0]
+            
+            if line == '    Or copy and paste one of these URLs:\n':
+                jupyter_vscode = lines[i+1][8:-1]
+                jupyter_local_url = lines[i+2][8:-1]
+        except:
+            pass
+        
+    #%%
+    ssh_command = lines[12].split(':')
+    ssh_command[1] = host_ip_address
+
+
     print("="*100)
     print("Run this command on your local terminal:")
     print(':'.join(ssh_command)[12:-1])
@@ -33,5 +34,6 @@ try:
     print("Visual Studio Code Jupyter Remote Serever:")
     print(jupyter_vscode)
     print("="*100)
+    
 except:
     print("Jupyter is opening.. Please wait for a few seconds..")
